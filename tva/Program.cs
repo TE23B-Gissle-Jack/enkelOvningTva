492f37c2c119c34a66f0d6c584f33947ad448685
Jack Gissle <jack.gissle@elev.ga.ntig.se>
2024-11-06 14:05:47 +0100

    first commit

---
+++ tva/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] lekia = ["vatten pistol", "hot wheels", "barbie", "bus light year", "woodie"];
+string[] namn = ["Andy", "Bert", "Karl", "Carl", "Max"];
+int[] betyg = [1,5,7,9,10];
+for(int i = 0; i < 10; i++) 
+{
+    Console.WriteLine($"{namn[Random.Shared.Next(5)]} ger {lekia[Random.Shared.Next(5)]} betyget {betyg[Random.Shared.Next(5)]}");
+
+}
+
+List<string> cities = [];
+string awns = "";
+while(awns != "exit")
+{
+    awns = Console.ReadLine().ToLower();
+    if(awns != "exit")
+    {
+        cities.Add(awns);
+    }
+}
+foreach(string name in cities)
+{
+    Console.WriteLine(name);
+}
+Console.ReadLine();
